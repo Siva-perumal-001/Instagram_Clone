e5feee91ba3418d72a59cf1c65453f8b0ef82fb8
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-03 21:42:07 +0530

    basic wireframe split up was done

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import '../Styles/index.css'
+
+function App() {
+  return (
+    <div className='main'>
+        <div className="side-bar">SIDEBAR</div>
+        <div className="feed">FEED</div>
+        <div className="suggestion">SUGGESTIONS</div>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './Styles/index.css'
+import App from './Components/App.jsx'
+import Sidebar from './Components/Sidebar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
