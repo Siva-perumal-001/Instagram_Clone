510e22ebb743e5dd4df5e8a6948051e88a8595eb
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-04 22:52:55 +0530

    sidebar was ready as frontend only

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,12 @@
 import '../Styles/index.css'
+import Feed from './Feed'
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className='main'>
-        <div className="side-bar">SIDEBAR</div>
-        <div className="feed">FEED</div>
+        <div className="side-bar"><Sidebar/></div>
+        <div className="feed"><Feed/></div>
         <div className="suggestion">SUGGESTIONS</div>
     </div>
   )
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import textLogo from '../assets/insta-text.png'
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <>
+      <div className='sd-top'>
+        <img src={textLogo} alt="Instagram-text-logo" />
+        <div><i class="ri-home-4-fill"></i>Home</div>
+        <div><i class="ri-search-line"></i>Search</div>
+        <div><i class="ri-compass-3-line"></i>Explore</div>
+        <div><i class="ri-movie-ai-line"></i>Reels</div>
+        <div><i class="ri-chat-ai-3-line"></i>Messages</div>
+        <div><i class="ri-heart-line"></i>Notifications</div>
+        <div><i class="ri-add-box-line"></i>Create</div>
+        <div><i class="ri-account-circle-fill"></i>Profile</div>
+      </div>
+      <div className='sd-btm'>
+        <div><i class="ri-threads-line"></i>Threads</div>
+        <div><i class="ri-menu-line"></i>More</div>
+      </div>
+    </>
   )
 }
 
