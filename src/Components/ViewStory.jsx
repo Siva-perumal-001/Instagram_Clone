806adaffc23d60b77639c6f3491fe8f5f2f046e8
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-07 00:04:48 +0530

    Story field was created and routes was set

--- src/Components/ViewStory.jsx
+++ src/Components/ViewStory.jsx
@@ -1,9 +1,36 @@
-
+import { useEffect, useState } from "react"
+import { useParams, Link, useNavigate } from "react-router-dom"
 
 function ViewStory() {
-  return (
-    <div>ViewStory</div>
-  )
+
+    const {id, tot} = useParams()
+    const [story , setStory] = useState(null)
+    const Navigate = useNavigate()
+
+    useEffect(()=>{
+    fetch(`http://localhost:3000/story/${id}`)
+        .then(data => data.json())
+            .then(data => setStory(data))
+                .catch(err => console.log(err))
+    },[id])
+
+    if(id > tot || id<=0){
+        Navigate('/')
+    }
+
+    return (
+        <div>
+            {story ? (
+            <div className='viewStory'>
+                <Link className="arrow" to={`http://localhost:5173/story/${Number(id)-1}/${tot}`}><i class="ri-arrow-left-s-line"></i></Link>
+                <img src={story.image} alt="Story" />
+                <Link className="arrow" to={`http://localhost:5173/story/${Number(id)+1}/${tot}`}><i class="ri-arrow-right-s-line"></i></Link>
+            </div>
+            ) : (
+            <div>loading</div>
+            )}
+        </div>
+    )
 }
 
 export default ViewStory
